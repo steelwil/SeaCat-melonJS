d8acb1ed2319ca7c67c01c80a5969026ea6a356c
William Bell <william.bell@frog.za.net>
2015-08-03 21:32:40 +0200

    update air indicator to be the same as the Ged version

--- js/entities/air_bubble_indicator.js
+++ js/entities/air_bubble_indicator.js
@@ -10,25 +10,34 @@ game.HUD.air_bubble_indicator = me.Renderable.extend({
         this.animationSheet.addAnimation("1", [54]);
         this.animationSheet.addAnimation("2", [55]);
         this.animationSheet.addAnimation("3", [56]);
+        this.animationSheet.addAnimation("4", [57]);
     },
 
-  draw : function(renderer) {
-    renderer.save();
-    var b = 6; // number of bubbles
-    var d = 100 / b; // amount of air per bubble
-    for (var i = 0; i < b; i++) {
-        var v = d + i * d - 0.000001; // subtract a tiny amount to solve rounding issues
-        this.animationSheet.pos.x = this.pos.x + 16 * i;
-        if (game.data.percentageAir >= v) {
-            this.animationSheet.setCurrentAnimation("0");
+    draw : function(renderer) {
+        renderer.save();
+        var b = 6; // number of bubbles
+        var d = 4.1666666;  // 100/4/b
+
+        renderer.save();
+        for (var i = 0; i < b; i++) {
+            this.animationSheet.pos.x = this.pos.x + 16 * i;
+            var animpos = 4;
+            if (game.data.percentageAir > 100 - i*4*d - d) {
+                animpos = 0;
+            }
+            else if (game.data.percentageAir > 100 - i*4*d - d*2) {
+                animpos = 1;
+            }
+            else if (game.data.percentageAir > 100 - i*4*d - d*3) {
+                animpos = 2;
+            }
+            else if (game.data.percentageAirr > 100 - i*4*d - d*4) {
+                animpos = 4;
+            }
+            this.animationSheet.setCurrentAnimation(animpos);
             this.animationSheet.draw(renderer);
         }
-        else if (game.data.percentageAir > 0 && game.data.percentageAir + d > v) {
-            var l = Math.floor((v - game.data.percentageAir) / d * 3 + 1);
-            this.animationSheet.setCurrentAnimation(l);
-            this.animationSheet.draw(renderer);
-        }
+        renderer.restore();
     }
-    renderer.restore();
-  }
 });
+
